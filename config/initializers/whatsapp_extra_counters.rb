a99bbc5e17bae6168b1751ce6193ded2fe1a1f76
X3r0_N1hil1st <131015384+Amilton-Portraite@users.noreply.github.com>
2026-06-02 13:57:05 +0200

    Add whatsapp extra counters initializer

---
+++ config/initializers/whatsapp_extra_counters.rb
@@ -0,0 +1,11 @@
+Decidim.stats.register :users_count,
+                               priority: StatsRegistry::HIGH_PRIORITY,
+                               icon_name: "user-line",
+                               tooltip_key: "users_count_tooltip" do |organization, start_at, end_at|
+          users = Decidim::Chatbot::Sender.where(decidim_user: organization.users)
+          users = users.where(created_at: @start_at..) if @start_at.present?
+          users = users.where(created_at: ..@end_at) if @end_at.present?
+          users.count
+        end
+
+        
