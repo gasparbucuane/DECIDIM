e33ae75b6ba4e1c996324e3bfaddeedd4535af53
X3r0_N1hil1st <131015384+Amilton-Portraite@users.noreply.github.com>
2026-06-02 14:45:35 +0200

    Update whatsapp_extra_counters.rb

--- config/initializers/whatsapp_extra_counters.rb
+++ config/initializers/whatsapp_extra_counters.rb
@@ -1,11 +1,10 @@
-Decidim.stats.register :users_count,
-                               priority: StatsRegistry::HIGH_PRIORITY,
-                               icon_name: "user-line",
-                               tooltip_key: "users_count_tooltip" do |organization, start_at, end_at|
-          users = Decidim::Chatbot::Sender.where(decidim_user: organization.users)
-          users = users.where(created_at: @start_at..) if @start_at.present?
-          users = users.where(created_at: ..@end_at) if @end_at.present?
-          users.count
-        end
 
-        
+Decidim.stats.register :whatsapp_users_count,
+                        priority: Decidim::StatsRegistry::HIGH_PRIORITY,
+                        icon_name: "user-line",
+                        tooltip_key: "whatsapp_users_count_tooltip" do |organization, start_at, end_at|
+  users = Decidim::Chatbot::Sender.where(decidim_user: organization.users)
+  users = users.where(created_at: start_at..) if start_at.present?
+  users = users.where(created_at: ..end_at) if end_at.present?
+  users.count
+end
