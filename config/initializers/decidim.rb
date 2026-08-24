0c4fb1751cb8ad48958daa6e51a6d6c18e2b3768
X3r0_N1hil1st <131015384+Amilton-Portraite@users.noreply.github.com>
2026-08-24 14:17:03 +0200

    Configure Decidim for Portuguese localization
    Set default locale to Portuguese and add available locales.

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -6,7 +6,12 @@ Rails.application.config.to_prepare do
     PasswordValidator.const_set(:MINIMUM_LENGTH, 6)
   end
 end
-
 Decidim.configure do |config|
   config.admin_password_min_length = 6
+  config.default_locale = :pt
+  config.available_locales = [:pt, :"pt-BR"]
+ end
+
+Rails.application.config.after_initialize do
+  I18n.fallbacks.map(pt: :"pt-BR")
 end
